b72e027e7f998ee9a17f07e1331dd0c28949f0d7
Jamie Taylor <me@nekodex.net>
2016-02-24 18:48:19 +0900

    Clean up presets

--- scthumbd.js
+++ scthumbd.js
@@ -11,36 +11,30 @@ var thumber = scThumber({
     'cover': {
       width: 900,
       height: 250,
-      quality: 95,
     },
     'cover@2x': {
       width: 1800,
-      height: 500,
-      quality: 95,
+      height: 500
     },
     // Beatmap card thumbnail
     // 800*200 (@2x)
     'card': {
       width: 400,
-      height: 100,
-      quality: 95,
+      height: 100
     },
     'card@2x': {
       width: 800,
-      height: 200,
-      quality: 95,
+      height: 200
     },
     // Beatmap list thumbnail
     // 160*100 (@2x)
     'list': {
       width: 80,
-      height: 50,
-      quality: 95,
+      height: 50
     },
     'list@2x': {
       width: 160,
-      height: 100,
-      quality: 95,
+      height: 100
     },
   }
 });
